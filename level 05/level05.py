75e2b7686c6916d37b7de6436cff7cbdbc97b41d
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-05-28 20:00:58 +0400

    home work

--- level 05/level05.py
+++ level 05/level05.py
@@ -3,5 +3,5 @@
 name="nikoloz"
 name="lasha"
 
-#debgging nishnavs kodshi shecdomebis gasworeba
+#debugging nishnavs kodshi shecdomebis gasworeba
 #komentars imitom viyenebt rom gavigot ra gviweria kodis garkveul nawilshi

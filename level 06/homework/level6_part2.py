44b445674a1f65e519b9a059562c0c5afe36bf15
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-06-01 12:45:52 +0400

    homework

---
+++ level 06/homework/level6_part2.py
@@ -0,0 +1,11 @@
+name = "nikoloz"
+surname = "qarcivadze"
+age = 11
+text = "I am programer"
+print("I am " + name + "my surname is" + surname + "my age is" + str(age) + "and " + text)
+
+
+fruit = "apple" 
+fruit = "banani"
+fruit = "orenge"
+print(fruit)

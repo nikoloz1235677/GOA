3ea77750fc62d1e1c7e76d979033253ea3cdfa92
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-07-16 18:24:37 +0400

    homework

---
+++ level 18/classwork/level18.py
@@ -0,0 +1,15 @@
+#range ის გამოყენებით გამოიტანეთ ტერმინალში 30ჯერ Hello World
+#2) range ის გამოყენებით გამოიტანეთ ტერმინალში 1დან 10მდე რიცხვები
+#3) range ის გამოყენებით გამოიტანეთ ტერმინალში 10დან 20მდე 2 ის ჯერადი რიცხვები (მინიშნება start,end,step)
+
+
+
+
+for i in range(30):
+    print("hellow world")
+
+for i in range(1,10):
+    print(i)
+
+for i in range(10,20,2):
+    print(i)

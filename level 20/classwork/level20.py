59d55e5caaba8d5f992cd4b8a7f422cd6a41b81d
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-07-22 20:41:59 +0400

    homework

---
+++ level 20/classwork/level20.py
@@ -0,0 +1,14 @@
+ #შექმენით ცვლადი age და if-else statement რომელიც დაადგენს მომხმარებელი
+ #  სრულ წლოვანია თუ არა, ანუ თუ მისი ასაკი მეტია ან ტოლი 18ის დაუწერეთ:
+ #  "You are adult". სხვა შემთხვევაში დაუწერეთ: "You aren't adult yet"
+     
+
+
+
+
+
+age = 22
+if age >= 18:
+ print("you are adult") 
+else:
+ print("you are not adult")

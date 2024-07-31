51f84a00020abc345466a1fa4649389a6717d004
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-07-31 11:22:09 +0400

    homework

---
+++ level22/classwork/level22.py
@@ -0,0 +1,43 @@
+#1) შექმენით 5 სხვადასხვა list'ი
+# , სხვადასხვა ელემენტების რაოდენობით და ყველას კომენტარის სახით უნდა დაუწეროთ საკუთარი ინდექსი
+  
+
+nayinebi =["barambo","tolia","gurjaani","panda","eskimo"]  
+#             0        1        2         3       4
+programing_language =["ccs","javascript","python","html","c++"]
+#                      0       1           2      3        4
+fruit =["apple","greap","watermelon","banana","strouber"]
+#       0           1       2            3        4
+sport =["karat","ragby","king box","basketball","volleball"]
+ #        0      1         2          3             4
+stites =["wikipedia","github"]
+ #           0         1
+
+
+
+
+
+
+-3         -2        -1
+shopping_list = ["bread", "chiken", "ketchup"]
+0         1          2
+print(shopping_list[1])
+print(shopping_list[-1])
+-3           -2           -1
+tools = ["hammer", "screwdriver", "nails"]
+0            1            2
+print(tools[2])
+print(tools[-2])
+-3              -2        -1
+games = ["car parking", "pubg", "brawl stars"]
+0                1          2
+print(games[2])
+print(games[-1])
+-4        -3      -2         -1
+directions = ["right", "left", "forward" , "back"]
+0         1        2         3
+print(directions[3])
+print(directions[-2])
+-2          -1
+sites = ["wikipedia", "github"]
+0           1

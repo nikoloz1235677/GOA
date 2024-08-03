5672d7b428a9c67212e3ceb6c653dcff8e61ab26
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-08-03 10:13:01 +0400

    homework

--- level22/classwork/level22.py
+++ level22/classwork/level22.py
@@ -2,42 +2,60 @@
 # , სხვადასხვა ელემენტების რაოდენობით და ყველას კომენტარის სახით უნდა დაუწეროთ საკუთარი ინდექსი
   
 
-nayinebi =["barambo","tolia","gurjaani","panda","eskimo"]  
-#             0        1        2         3       4
-programing_language =["ccs","javascript","python","html","c++"]
-#                      0       1           2      3        4
-fruit =["apple","greap","watermelon","banana","strouber"]
-#       0           1       2            3        4
-sport =["karat","ragby","king box","basketball","volleball"]
- #        0      1         2          3             4
-stites =["wikipedia","github"]
- #           0         1
+list1 = ["car", "plane","metro","bus",]
+#          0       1       2      3
+#          -4      -3     -2     -1                
+list2 = ["big house","small house","apartament"]
+#             0            1               2
+#             -3           -2             -1     
+list3 = ["pants","shirt","shoes",]
+#           0       1       2
+#           -3      -2      -1  
+list4 = ["pen","pencil","ruler"]
+#          0        1       2
+#          -3       -2      -1  
+
+print(list1[0])
+print(list1[1])
+print(list1[2])
+print(list1[3])
+print(list1[-1])
+print(list1[-2])
+print(list1[-3])
+print(list1[-4])
 
 
+print(list2[0])
+print(list2[1])
+print(list2[2])
+print(list2[-1])
+print(list2[-2])
+print(list2[-3])
+
+print(list3[0])
+print(list3[1])
+print(list3[2])
+print(list3[-1])
+print(list3[-2])
+print(list3[-3])
+
+print(list4[0])
+print(list4[1])
+print(list4[2])
+print(list4[-1])
+print(list4[-2])
+print(list4[-3])
 
 
+listx = ["dad","mom","sister","brother","me"]
 
+listx[0] = "grandfather"
+listx[1] = "grandmother"
+listx[2] = "aunt"
+listx[3] = "uncle"
+listx[4] = "son"
 
--3         -2        -1
-shopping_list = ["bread", "chiken", "ketchup"]
-0         1          2
-print(shopping_list[1])
-print(shopping_list[-1])
--3           -2           -1
-tools = ["hammer", "screwdriver", "nails"]
-0            1            2
-print(tools[2])
-print(tools[-2])
--3              -2        -1
-games = ["car parking", "pubg", "brawl stars"]
-0                1          2
-print(games[2])
-print(games[-1])
--4        -3      -2         -1
-directions = ["right", "left", "forward" , "back"]
-0         1        2         3
-print(directions[3])
-print(directions[-2])
--2          -1
-sites = ["wikipedia", "github"]
-0           1
+str = ["hello"]
+str[0] = "o"
+
+print(str)

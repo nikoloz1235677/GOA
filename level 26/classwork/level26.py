b962d29a96965652a4531e152b6d49271cc78021
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-08-15 22:49:26 +0400

    homework

---
+++ level 26/classwork/level26.py
@@ -0,0 +1,16 @@
+def greeting():
+    print("Hello!")
+greeting()
+
+  
+def greeting():
+    print("I wish you success")
+greeting()
+
+
+def greeting():
+    print("You now have a lesson in Goa")
+greeting()
+
+
+

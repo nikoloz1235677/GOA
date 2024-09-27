99045175a9ce342fe105a1104fcba76c483f6a40
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-09-27 15:06:38 +0400

    homework

---
+++ level 38/homework/less.py
@@ -0,0 +1,12 @@
+# 2) შექმენით სია და შეინაზეთ სიტყვები, შემდეგ join ფუნქციის გამ,ოყენებით დაბეჭდეთ 
+# სამი სახით, ჯერ ჩვეულებრივ, შემდეგ '-' ებით და შემდეგ '_' ებით ყოველ სიტყვას შორის.
+
+lst = ["cixe","zona","gelabarkalaia"]
+join= ' '.join(lst)
+print(join)
+
+djoin= '-'.join(lst)
+print(djoin)
+
+ujoin = '_'.join(lst)
+print(ujoin) 

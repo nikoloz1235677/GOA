618be55b569d110f7e46dbb9a2ba8b1d644dd5fe
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-10-24 21:35:52 +0400

    homework

--- level 43/homework/hw.py
+++ level 43/homework/hw.py
@@ -2,8 +2,57 @@
 # 3) გამოიტანეთ ამ 5'ივე dictionary'ს key list'ები
 # 4) გამოიტანეთ ამ 5'ივე dictionary'ს value list'ები
 # 5) გამოიტანეთ ამ 5'ივე dictionary'ს key:value pair list'ები
-# 6) for loop'ის მეშვეობით გამოიტანეთ ყველას Value'ები 3'ივე გზის გამოყენებით რომელიც გაკვეთილზე ავხსენით
+# 6) for loop'ის მეშვეობით გამოიტანეთ ყველას Value'ები 3'ივე გზის გამოყენებით რომელიც გაკვეთილზე ავხსენი
 
+cars ={
+    "model": "bmw",
+    "speed": "360",
+    "zravi":"2.0 turbo"
+
+}
+print(cars)
+
+
+
+cars ={
+    "model": "mercedes",
+    "speed": "260",
+    "zravi":"1.9 "
+}
+
+print(cars)
+
+
+
+
+cars ={
+    "model": "lamborghin",
+    "speed": "360",
+    "zravi":"3.0turbo"
+
+}
+print(cars)
+
+
+
+
+cars ={
+    "model": "jiguli",
+    "speed": "100",
+    "zravi":"1.0"
+
+}
+print(cars)
+
+
+
+cars ={
+    "model": "aud",
+    "speed": "180",
+    "zravi":"2.0 "
+
+}
+print(cars)
 
 
 
--- level 45/classwork/cw.py
+++ level 45/classwork/cw.py
@@ -1,1 +1,42 @@
-{keyboard,mouse, camputer,}
+fruit={
+    "gemrieli":"100%",
+    "xilissaxeoba":"banani",
+    "sadisxams":"xeze",
+    "ra chams":"maimuni ",
+    "ratom aris didi":"imitom didis partia",
+    "uyvars ":"adamianebs",
+    "ratom chamen cxovelebi banans":"kaia da imitom",
+    "romel wels gamovida":"1768",
+    "saidan agmochnda":"filipinida ",
+}
+for key,i,in fruit.items():
+    print(key)
+
+
+
+
+    kompiuteri={
+        "modeli":"mackbook pro",
+        "modeli":"mackbook m2",
+        "modeli":"acer windows11",
+        "modeli":"acer samsung",
+        "modeli":"acer",
+        "modeli":"skolis buki",
+        "modeli":"asusi v2 book",
+        "modeli":"mackbook air m2",
+        "modeli":"lenovo",
+        "modeli":"macbok",
+
+    }
+    for key,value,in kompiuteri.items():
+        print(value)
+
+result = [(x // 2) + 1 for x in range(50, 101)]
+print(result)
+num1= [(x // 2) + 1 for x in range(50, 101)]
+print(num1)
+num2 = [(x * 2) + 1 for x in range(50, 101)]
+print(num2)
+
+
+

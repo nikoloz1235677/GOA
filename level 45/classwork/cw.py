d4e97f04308c1f15621d258a7df688bc3509f6a5
nikoloz1235677 <168955229+nikoloz1235677@users.noreply.github.com>
2024-10-31 19:12:56 +0400

    homework

---
+++ level 45/classwork/cw.py
@@ -0,0 +1,69 @@
+opel = {
+    "opeli": "1999",
+    "gamoshveba": 2000,
+    "speed": "20000m"
+}
+
+merceds = {
+    "mercedes": "idk man",
+    "gamoshveba": 2010,
+    "speed": "200m"
+}
+
+bmw = {
+    "bmw": "m5",
+    "gamoshveba": 2024,
+    "speed": "200000000m"
+}
+
+
+
+
+for i in opel.items():
+    print(i)
+
+for i in merceds.items():
+    print(i)
+
+for i in bmw.items():
+    print(i)
+
+
+
+
+
+    numbers = list(range(1, 1001))
+
+
+
+
+    opel = {
+    "opeli": "1999",
+    "gamoshveba": 2000,
+    "speed": "20000m"
+}
+
+merceds = {
+    "mercedes": "idk man",
+    "gamoshveba": 2010,
+    "speed": "200m"
+}
+
+bmw = {
+    "bmw": "m5",
+    "gamoshveba": 2024,
+    "speed": "200000000m"
+}
+
+for i in opel.values():
+    print(i)
+
+for i in merceds.values():
+    print(i)
+
+for i in bmw.values():
+    print(i)
+
+
+
+num = [i * 2 for i in range(1, 101)]
